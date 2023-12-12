466e78931ed35f95417ab9f3b43f8cc72daf8f63
swats-the-floran <euc.anatoly@gmail.com>
2023-12-13 01:47:05 +0500

    rewrited api

--- profiles_service/profiles/api/v1/urls.py
+++ profiles_service/profiles/api/v1/urls.py
@@ -1,38 +1,25 @@
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import SimpleRouter, NestedSimpleRouter
 
 from .views import (
-    BookmarkAPIView,
-    PersonAPIView,
-    DeactivatePersonAPIView,
-    FavoriteAPIView,
-    BookmarksListView,
-    FavoritesListView,
-    PersonViewSet
+    PersonViewSet,
+    BookmarkViewSet,
+    FavoriteViewSet,
 )
 
 
 router = SimpleRouter()
-router.register('users', PersonViewSet)
+router.register('persons', PersonViewSet)
+
+persons_router = NestedSimpleRouter(router, r'persons', lookup='person')
+persons_router.register(r'bookmarks', BookmarkViewSet, basename='user-bookmarks')
+persons_router.register(r'favorites', FavoriteViewSet, basename='user-favorites')
 
 
-urlpatterns = router.urls
-urlpatterns += [
-    # profiles
-    # path('users/', PersonAPIView.as_view(), name='create_user'),
-    # path('users/whoami/', PersonAPIView.as_view(), name='get_user_info'),
-    # path('users/<uuid:uuid>/', PersonAPIView.as_view(), name='update_user'),
-    # path('users/<uuid:uuid>/deactivate/', DeactivatePersonAPIView.as_view(), name='deactivate_user'),
-    # path('users/<uuid:uuid>/detail/', PersonAPIView.as_view(), name='get_detailed_user_info'),
-    # bookamarks
-    # path('users/<uuid:uuid>/bookmarks/', BookmarkAPIView.as_view(), name='bookmarks'),
-    # path('users/<uuid:uuid>/bookmarks/list', BookmarksListView.as_view(), name='bookmarks_list'),
-    # favorites
-    # path('users/favorite_movies/', FavoriteAPIView.as_view(), name='favorite_movie_create'),
-    # path('users/favorite_movies/<int:favorite_movie_id>/', FavoriteAPIView.as_view(), name='favorite_movie_delete'),
-    # path('users/<uuid:uuid>/favorites/list/', FavoritesListView.as_view(), name='favorites_list'),
-    # reviews
+urlpatterns = [
+    path(r'', include(router.urls)),
+    path(r'', include(persons_router.urls)),
     # openapi
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('openapi-profiles/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
