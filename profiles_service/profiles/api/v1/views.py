9bee61497ebac745ea981c1cbc027757a7d440a3
swats-the-floran <euc.anatoly@gmail.com>
2023-12-16 05:45:17 +0500

    added authentiaction

--- profiles_service/profiles/api/v1/views.py
+++ profiles_service/profiles/api/v1/views.py
@@ -1,7 +1,10 @@
-from typing import OrderedDict
+import re
+import uuid
 from drf_spectacular.utils import extend_schema, extend_schema_view
-import requests
+from typing import OrderedDict
 
+import requests
+import jwt
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,7 +13,6 @@ from profiles.api.v1.serializers import (
     BookmarkSerializer,
     FavoriteSerializer,
     FilmReviewSerializer,
-    FilmSerializer,
     PersonDetailSerializer,
     PersonSerializer,
 )
@@ -21,6 +23,48 @@ from profiles.models import (
     Person,
 )
 from profiles.utils import StandardResultsSetPagination
+
+from rest_framework.authentication import get_authorization_header
+from rest_framework.permissions import BasePermission
+
+
+class IsAuthenticated(BasePermission):
+
+    def has_permission(self, request, view):
+        # TODO: exceptions with messages
+
+        #if endpoint does not require user id, there is no need for permission
+        path = request.get_full_path()
+        person_pk = re.search('[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}', path)
+        if person_pk is None:
+            return True
+
+        # in other case we check credentials correcttness
+        auth = get_authorization_header(request).split()
+        if not auth or auth[0].decode().lower() != 'bearer' or len(auth) != 2:
+            return False
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            return False
+
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'X-Request-Id': str(uuid.uuid4()),
+        }
+        resp = requests.get('http://auth_service:8080/api/v1/auth/me', headers=headers)
+
+        if not resp.ok:
+            return False
+
+        # check if token belongs to the user
+        token_email = jwt.decode(token, key='secret', algorithms=['HS256'])['email']
+        person_obj = Person.objects.filter(pk=person_pk).first()
+        if person_obj and person_obj.email == token_email:
+            return False
+
+        return False
 
 
 @extend_schema_view(
@@ -46,7 +90,7 @@ class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all().filter(is_active=True)
     serializer_class = PersonSerializer
     pagination_class = StandardResultsSetPagination
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     @staticmethod
     def _get_reviews_likes(reviews_data: OrderedDict) -> OrderedDict:
@@ -136,7 +180,7 @@ class BookmarkViewSet(viewsets.ModelViewSet):
 
     serializer_class = BookmarkSerializer
     pagination_class = StandardResultsSetPagination
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return Bookmark.objects.filter(person=self.kwargs['person_pk'])
@@ -157,7 +201,7 @@ class FavoriteViewSet(viewsets.ModelViewSet):
 
     serializer_class = FavoriteSerializer
     pagination_class = StandardResultsSetPagination
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return Favorite.objects.filter(person=self.kwargs['person_pk'])
@@ -185,7 +229,7 @@ class FilmReviewViewSet(viewsets.ModelViewSet):
 
     serializer_class = FilmReviewSerializer
     pagination_class = StandardResultsSetPagination
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return FilmReview.objects.filter(person=self.kwargs['person_pk'])
